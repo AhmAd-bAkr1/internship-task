21ea1ab81057e2d93facdb17db49a7a3d5bb4cc3
AhmAd-bAkr1 <msd39777@gmail.com>
2023-03-27 05:24:19 +0200

    Done

--- js/main.js
+++ js/main.js
@@ -33,4 +33,71 @@ let counter = setInterval(function() {
       }
 }
 }, 1000);
-console.log(counter)
+
+const moreBtn = document.querySelector('.coontainer .mob-sec2 .cards button')
+let currentItem = 2
+moreBtn.addEventListener("click" , function (e) {
+  let elementBox = [...document.querySelectorAll('.coontainer .mob-sec2 .cards .crd')]
+  for (let i = currentItem; i < currentItem + 2; i++) {
+    setTimeout(() => {
+      if (elementBox[i]) {
+              elementBox[i].style.display = "flex"
+  
+      }
+    }, 1000);
+  }  
+  currentItem += 2
+  
+  if (currentItem >= elementBox.length) {
+    e.target.classList.add("loaded")
+    
+  }
+  })
+
+const loadBtn = document.querySelector("#loader")
+const loadBtn1 = document.querySelector("#loader1")
+const secCard = document.querySelector(".coontainer .sec5 .cerds .main-crd2")
+const currentItem1 = 1
+const currentItem2 = 2
+
+loadBtn.addEventListener("click" , function (e) {
+let elementBox = [...document.querySelectorAll('.coontainer .sec5 .cerds .main-crd')]
+e.target.classList.add("show")
+for (let i = currentItem1; i < currentItem1 + 1; i++) {
+  setTimeout(() => {
+    e.target.classList.remove("show")
+    if (elementBox[i]) {
+            elementBox[i].style.display = "block"
+            elementBox[i].style.position = "inherit"
+
+    }
+  }, 1000);
+}  
+currentItem1 += 1
+
+if (currentItem1 >= elementBox.length) {
+  e.target.classList.add("loaded")
+  
+}
+})
+
+loadBtn1.addEventListener("click" , function (e) {
+  let elementBox = [...document.querySelectorAll('.coontainer .sec5 .cerds .main-crd')]
+  e.target.classList.add("show")
+  for (let i = currentItem2; i < currentItem2 + 1; i++) {
+    setTimeout(() => {
+      e.target.classList.remove("show")
+      if (elementBox[i]) {
+        elementBox[i].style.display = "block"
+        elementBox[i].style.position = "inherit"
+        secCard.style.display = "block"
+
+      }
+    }, 1000);
+  }  
+  currentItem2 += 1
+  if (currentItem2 >= elementBox.length) {
+    e.target.classList.add("loaded")
+    
+  }
+  })
